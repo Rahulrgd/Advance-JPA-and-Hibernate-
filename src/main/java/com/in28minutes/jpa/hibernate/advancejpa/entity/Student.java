f2f7feecfc8814c5f73bb567d5979ff906199af6
RahulGupta <rahulrgd21@gmail.com>
2024-02-17 01:31:31 +0530

    Step 23 - Introduction to One to One Relationship

--- src/main/java/com/in28minutes/jpa/hibernate/advancejpa/entity/Student.java
+++ src/main/java/com/in28minutes/jpa/hibernate/advancejpa/entity/Student.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 
 @Entity
 public class Student {
@@ -14,6 +15,9 @@ public class Student {
 
   @Column(nullable = false)
   private String name;
+
+  @OneToOne
+  private Passport passport;
 
   protected Student() {}
 
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,13 +2,13 @@ insert into course(id, fullname, created_date, last_updated_date) values(10001, 
 insert into course(id, fullname, created_date, last_updated_date) values(10002, 'MicroServices in 100 steps', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
 insert into course(id, fullname, created_date, last_updated_date) values(10003, 'SQL in 50 steps', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
 
-insert into student(id, name) values(20001, 'Rahul');
-insert into student(id, name) values(20002, 'Rahi');
-insert into student(id, name) values(20003, 'Satendra');
-
 insert into passport(id, number) values(40001, 'E123456');
 insert into passport(id, number) values(40002, 'N123456');
 insert into passport(id, number) values(40003, 'C123456');
+
+insert into student(id, name, passport_id) values(20001, 'Rahul', 40001);
+insert into student(id, name, passport_id) values(20002, 'Rahi', 40002);
+insert into student(id, name, passport_id) values(20003, 'Satendra', 40003);
 
 insert into review(id, rating, description) values(50001, 5, 'Great Course');
 insert into review(id, rating, description) values(50002, 4, 'Good Course');
