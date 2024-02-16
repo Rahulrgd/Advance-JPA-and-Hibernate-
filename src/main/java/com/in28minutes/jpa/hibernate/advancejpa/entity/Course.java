651fc00e0182362cf18d843f88f393a4c6e18e9b
RahulGupta <rahulrgd21@gmail.com>
2024-02-16 22:28:41 +0530

    Step 19 - JPA and Hibernate Annotations - @NamedQuery and @NamedQueries

--- src/main/java/com/in28minutes/jpa/hibernate/advancejpa/entity/Course.java
+++ src/main/java/com/in28minutes/jpa/hibernate/advancejpa/entity/Course.java
@@ -1,18 +1,30 @@
 package com.in28minutes.jpa.hibernate.advancejpa.entity;
 
-import java.time.LocalDateTime;
-
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.NamedQuery;
 import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 @Entity
 // @Table(name = "CourseDetails")
+@NamedQueries(
+  value = {
+    @NamedQuery(
+      name = "query_get_all_courses",
+      query = "select c from Course c"
+    ),
+    @NamedQuery(
+      name = "query_courses_where_id>2",
+      query = "Select c from Course c where id>3"
+    ),
+  }
+)
 public class Course {
 
   @Id
--- src/test/java/com/in28minutes/jpa/hibernate/advancejpa/repository/JPQLTests.java
+++ src/test/java/com/in28minutes/jpa/hibernate/advancejpa/repository/JPQLTests.java
@@ -3,8 +3,11 @@ package com.in28minutes.jpa.hibernate.advancejpa.repository;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
+import com.in28minutes.jpa.hibernate.advancejpa.entity.Course;
+import com.in28minutes.jpa.hibernate.advancejpa.repository.CourseRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import java.util.List;
-
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,39 +15,39 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 
-import com.in28minutes.jpa.hibernate.advancejpa.entity.Course;
-import com.in28minutes.jpa.hibernate.advancejpa.repository.CourseRepository;
-
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.TypedQuery;
-
 @SpringBootTest
-class JPQLTests{
+class JPQLTests {
 
-	private Logger log = LoggerFactory.getLogger(this.getClass());
+  private Logger log = LoggerFactory.getLogger(this.getClass());
 
-	@Autowired
-	EntityManager em;
+  @Autowired
+  EntityManager em;
 
-	@Test
-	public void jpql_basic() {
-		List resultList = em.createQuery("Select c from Course c").getResultList();
-		log.info("Select c from Course c -> {}", resultList);
-	}
+  @Test
+  public void jpql_basic() {
+    List resultList = em
+      .createNamedQuery("query_get_all_courses")
+      .getResultList();
+    log.info("Select c from Course c -> {}", resultList);
+  }
 
-	@Test
-	public void jpql_typed() {
-		TypedQuery<Course> query = em.createQuery("Select c from Course c", Course.class);
-		List<Course> resultList = query.getResultList();
-		log.info("Select c from Course c -> {}", resultList);
-	}
+  @Test
+  public void jpql_typed() {
+    TypedQuery<Course> query = em.createNamedQuery(
+      "query_get_all_courses",
+      Course.class
+    );
+    List<Course> resultList = query.getResultList();
+    log.info("Select c from Course c -> {}", resultList);
+  }
 
-	@Test
-	public void jpql_where() {
-		TypedQuery<Course> query = em.createQuery("Select c from Course c where id>3", Course.class);
-		List<Course> resultList = query.getResultList();
-		log.info("Select c from Course c where id>3 -> {}", resultList);
-	}
-
-	
+  @Test
+  public void jpql_where() {
+    TypedQuery<Course> query = em.createNamedQuery(
+      "query_courses_where_id>2",
+      Course.class
+    );
+    List<Course> resultList = query.getResultList();
+    log.info("Select c from Course c where id>3 -> {}", resultList);
+  }
 }
