1170c647e3597fe248018cc690c8b74cf41be53d
RahulGupta <rahulrgd21@gmail.com>
2024-02-16 06:28:57 +0530

    Step 07 - Writing Unit Test for deleteById method

--- src/test/java/com/in28minutes/jpa/hibernate/advancejpa/repository/CourseRepositoryTests.java
+++ src/test/java/com/in28minutes/jpa/hibernate/advancejpa/repository/CourseRepositoryTests.java
@@ -1,12 +1,14 @@
 package com.in28minutes.jpa.hibernate.advancejpa.repository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 
 import com.in28minutes.jpa.hibernate.advancejpa.entity.Course;
 import com.in28minutes.jpa.hibernate.advancejpa.repository.CourseRepository;
@@ -20,9 +22,16 @@ class CourseRepositoryTests{
 	CourseRepository repository;
 
 	@Test
-	public void contextLoads() {
-		Course course = repository.findById(10001L);
-		assertEquals("AWS", course.getName());
+	public void findById_basic() {
+		Course course = repository.findById(10003L);
+		assertEquals("DevOps", course.getName());
+	}
+
+	@Test
+	@DirtiesContext
+	public void deleteById_basic() {
+		repository.deleteById(10002L);
+		assertNull(repository.findById(10002L));
 	}
 
 }
