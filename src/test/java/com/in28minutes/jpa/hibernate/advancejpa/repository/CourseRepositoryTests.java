76c803cab7fc27694e55bcd61e59ef80ae15acb6
RahulGupta <rahulrgd21@gmail.com>
2024-02-19 04:41:03 +0530

    Step 72 - Hibernate and JPA Caching - First Level Cache

--- src/test/java/com/in28minutes/jpa/hibernate/advancejpa/repository/CourseRepositoryTests.java
+++ src/test/java/com/in28minutes/jpa/hibernate/advancejpa/repository/CourseRepositoryTests.java
@@ -3,78 +3,82 @@ package com.in28minutes.jpa.hibernate.advancejpa.repository;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
+import com.in28minutes.jpa.hibernate.advancejpa.entity.Course;
+import com.in28minutes.jpa.hibernate.advancejpa.entity.Review;
+import com.in28minutes.jpa.hibernate.advancejpa.repository.CourseRepository;
+import jakarta.persistence.EntityManager;
 import java.util.List;
-
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
-
-import com.in28minutes.jpa.hibernate.advancejpa.entity.Course;
-import com.in28minutes.jpa.hibernate.advancejpa.entity.Review;
-import com.in28minutes.jpa.hibernate.advancejpa.repository.CourseRepository;
-
-import jakarta.persistence.EntityManager;
-import jakarta.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
-class CourseRepositoryTests{
+class CourseRepositoryTests {
 
-	private Logger log = LoggerFactory.getLogger(this.getClass());
+  private Logger log = LoggerFactory.getLogger(this.getClass());
 
-	@Autowired
-	CourseRepository repository;
+  @Autowired
+  CourseRepository repository;
 
-	@Autowired
-	EntityManager em;
+  @Autowired
+  EntityManager em;
 
-	@Test
-	public void findById_basic() {
-		Course course = repository.findById(10003L);	}
+  @Test
+  public void findById_basic() {
+    Course course = repository.findById(10003L);
+  }
 
-	@Test
-	@DirtiesContext
-	public void deleteById_basic() {
-		repository.deleteById(10002L);
-		assertNull(repository.findById(10002L));
-	}
+  @Test
+  @Transactional
+  public void findById_firstLevelCacheDemo() {
+    Course course = repository.findById(10003L);
+	log.info("First Course Retrived -> {}", course);
+    Course course1 = repository.findById(10003L);
+	log.info("First Course Retrived again -> {}", course1);
+  }
 
-	@Test
-	@DirtiesContext
-	public void save_basic() {
-		// get a customer
-		Course course = repository.findById(10003L);
-		assertEquals("SQL in 50 steps", course.getName());
-		// update details
-		course.setName("DevOps-Updated");
-		// check value
-		repository.save(course);
-		Course course1 = repository.findById(10003L);
-		assertEquals("DevOps-Updated", course1.getName());
-	}
+  @Test
+  @DirtiesContext
+  public void deleteById_basic() {
+    repository.deleteById(10002L);
+    assertNull(repository.findById(10002L));
+  }
 
-	@Test
-	@DirtiesContext
-	public void playWithEntityManager(){
-		repository.playWithEntityManager();
-	}
+  @Test
+  @DirtiesContext
+  public void save_basic() {
+    // get a customer
+    Course course = repository.findById(10003L);
+    assertEquals("SQL in 50 steps", course.getName());
+    // update details
+    course.setName("DevOps-Updated");
+    // check value
+    repository.save(course);
+    Course course1 = repository.findById(10003L);
+    assertEquals("DevOps-Updated", course1.getName());
+  }
 
-	@Test
-	@Transactional
-	public void retriveReviewsForCourses(){
-		Course course = repository.findById(10003L);
-		log.info("Course Reviews-> {}", course.getReview());
-		
-	}
+  @Test
+  @DirtiesContext
+  public void playWithEntityManager() {
+    repository.playWithEntityManager();
+  }
 
-	@Test
-	// @Transactional
-	public void retriveCourseForReviews(){
-		Review review = em.find(Review.class, 50001L);
-		log.info("Review Course-> {}", review.getCourse());
-		
-	}
+  @Test
+  @Transactional
+  public void retriveReviewsForCourses() {
+    Course course = repository.findById(10003L);
+    log.info("Course Reviews-> {}", course.getReview());
+  }
 
+  @Test
+  // @Transactional
+  public void retriveCourseForReviews() {
+    Review review = em.find(Review.class, 50001L);
+    log.info("Review Course-> {}", review.getCourse());
+  }
 }
